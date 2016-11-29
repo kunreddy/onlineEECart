8e0ad3f913d42c87199a505dabcc7c6a07cbf513
kunreddy <kunreddyayyappareddy25@gmail.com>
2016-11-29 16:21:01 +0530

    Add files via upload

---
+++ HomeController.java
@@ -0,0 +1,14 @@
+package com.niit.Fitness.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+	
+	@RequestMapping("/")
+	public String home() {
+		return "home";
+	}
+
+}
